9145d864db92fe793d36388f0bb41e085f0d4e34
MatthewCQSZ <chqzhu@umd.edu>
2022-12-18 17:28:39 -0500

    Update README

--- tests/transfer_drl_gym_test.py
+++ tests/transfer_drl_gym_test.py
@@ -14,7 +14,7 @@ class TransferAlgorithmTester(unittest.TestCase):
         Set up the TransferDRLGym class.
         Check if the environment created is Gym environment.
         """
-        self.transfer_learning = TransferDRLGym(logdir="./tests/test_log", num_steps = 2000)
+        self.transfer_learning = TransferDRLGym(logdir="./test_log", num_steps = 2000)
         self.transfer_learning.make()
         self.assertTrue(isinstance(self.transfer_learning.env, gym.Env))
         
@@ -23,7 +23,7 @@ class TransferAlgorithmTester(unittest.TestCase):
         Test training using our env and RL algorithm, may take a while.
         Check if there's model saved at the end of training.
         """
-        self.transfer_learning = TransferDRLGym(logdir="./tests/test_log", num_steps = 2000)
+        self.transfer_learning = TransferDRLGym(logdir="./test_log", num_steps = 2000)
         self.transfer_learning.make()
         print(f"\nTesting training on {self.transfer_learning.env_name} with {self.transfer_learning.algorithm}, this may take a few minutes...")
         self.transfer_learning.train()
@@ -35,7 +35,7 @@ class TransferAlgorithmTester(unittest.TestCase):
         Test training using our env and our Policy Reuse algorithm, may take a while.
         Check if there's model saved at the end of training.
         """
-        self.transfer_learning = TransferDRLGym(logdir="./tests/test_log", algorithm = "SACPolicyReuse", num_steps = 2000)
+        self.transfer_learning = TransferDRLGym(logdir="./test_log", algorithm = "SACPolicyReuse", num_steps = 2000)
         self.transfer_learning.make()
         # Use one of our pre-trained policy for reuse
         self.transfer_learning.reuse_path = "../report_log/default_log_Nov25_Lift_vanilla_sac/BaseLift_Panda_SEED69/SAC/"
